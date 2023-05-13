97abd4eb66d2fc5d09beabb1832ff447c4a7086a
babs2020 <68337480+babs2020@users.noreply.github.com>
2023-05-13 19:38:27 +0000

    Add files via upload

---
+++ application/views/menu.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Oumy Hopital</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="menu.css" />
+  </head>
+  <body >
+    <div class=" align-items-center">
+   <section class="container-fluid bg-image m-0">
+    <div class="header ">
+     <!-- class:custom-menu pour avoir le menu en menu burger-->
+     <nav
+     class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top"
+     style="background-color: rgba(0, 0, 0, 0.5) !important"
+   >
+     <div class="container">
+       <h1 id="logo" class="text-light">KAYE<b>FA</b>DJOU</h1>
+       <button
+         class="navbar-toggler"
+         type="button"
+         data-bs-toggle="collapse"
+         data-bs-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+           <li class="nav-item">
+             <a class="nav-link active" id="home" style="border: 1px solid #4bcaff;" aria-current="page" href="#">HOME</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">SERVICES</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">FEATURES</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">PORTFOLIO</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">CONTACT</a>
+           </li>
+         </ul>
+       </div>
+     </div>
+   </nav>
+</div>
+</div>
+   </section>
+  
+  
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+      crossorigin="anonymous"
+    ></script>
+
+  </body>
+</html>
